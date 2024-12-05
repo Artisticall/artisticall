0b5e3baa50088babfaccb08fba0c9a9f144af3ac
felipeperezleal <seperezl@unal.edu.co>
2024-12-05 09:39:31 -0500

    Login update, logo added

--- app/src/main/java/com/dadm/artisticall/login/LoginScreen.kt
+++ app/src/main/java/com/dadm/artisticall/login/LoginScreen.kt
@@ -1,9 +1,15 @@
 package com.dadm.artisticall.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -15,12 +21,23 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import android.widget.Toast
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
+import com.dadm.artisticall.R
 import kotlinx.coroutines.launch
+
 
 @Composable
 fun LoginScreen(
@@ -30,30 +47,76 @@ fun LoginScreen(
     val googleSignInClient = remember { GoogleSignInClient(context) }
     var isSignedIn by remember { mutableStateOf(false) }
     val lifecycleOwner = LocalLifecycleOwner.current
+    var username by remember { mutableStateOf(TextFieldValue()) }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background),
         contentAlignment = Alignment.Center
     ) {
-        if (isSignedIn) {
-            navController.navigate("lobby_screen")
-        } else {
-            OutlinedButton(onClick = {
-                lifecycleOwner.lifecycleScope.launch {
-                    isSignedIn = googleSignInClient.signIn()
-                }
-            }) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(280.dp)
+                    .padding(bottom = 32.dp)
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
                 Text(
-                    text = "Sign In with Google",
+                    text = "Invitado",
                     fontSize = 16.sp,
-                    modifier = Modifier.padding(
-                        horizontal = 24.dp,
-                        vertical = 4.dp
-                    )
+                    color = Color.White,
+                    modifier = Modifier.padding(16.dp)
                 )
+                VerticalDivider(color = Color.Gray, modifier = Modifier.height(48.dp))
+                OutlinedButton(
+                    onClick = {
+                        lifecycleOwner.lifecycleScope.launch {
+                            isSignedIn = googleSignInClient.signIn()
+                            if (isSignedIn) {
+                                navController.navigate("lobby_screen")
+                            }
+                        }
+                    }
+                ) {
+                    Text(text = "Sign In with Google", fontSize = 16.sp)
+                }
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it},
+                label = { Text("Nickname") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp),
+                singleLine = true
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {
+                    if (username.text.isNotEmpty()) {
+                        navController.navigate("lobby_screen")
+                    } else {
+                        Toast.makeText(context, "Por favor, ingresa un nickname", Toast.LENGTH_SHORT).show()
+                    }
+                },
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                Text(text = "Jugar", fontSize = 16.sp)
             }
         }
     }
-
 }
